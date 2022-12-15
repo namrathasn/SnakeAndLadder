f636498915d5e45057ea75e2c8648bb0d8769718
namrathasn <namrathasn199819@gmail.com>
2022-12-15 23:16:24 +0530

    new

---
+++ UC5.java
@@ -0,0 +1,26 @@
+public class UC5 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake And Ladder Program");
+        {
+            int random_number = 0;
+            int firstSnake_Position = 25;
+            int secondSnake_Position = 78;
+            while (random_number < 100) {
+                int random_Check = (int) Math.floor(Math.random() * 10) % 6;
+                if (random_Check != 0) {
+                    random_number = random_number + random_Check;
+                }
+                if (random_number == firstSnake_Position) {
+                    System.out.println("Dies Moved to Snakes Position and moved towards backward direction:");
+                    random_number = 0;
+                } else if (random_number == secondSnake_Position) {
+                    System.out.println("Dies Moved to Snakes Position and moved towards backward direction:");
+                    random_number = 0;
+                }
+                if (random_Check != 0) {
+                    System.out.println("Ladder got a Number : " + random_Check + " My Ladder Moved to the Position : " + random_number);
+                }
+            }
+        }
+    }
+}
